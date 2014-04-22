d49f13df682d0aaeea6003cce68ba8ba50d72d60
Mike Jensen <jjensen.mike@gmail.com>
2014-04-22 10:32:49 -0400

    Finished minimizer functionality

--- test.py
+++ test.py
@@ -1,8 +1,36 @@
 from argparse import ArgumentParser
+from csv import DictReader, DictWriter
+import subprocess
+from os import remove
 
 """
   Enter command line params and other stuff
 """
+
+
+def cut_files(fname, fields):
+    print 'Cutting {}...'.format(fname)
+    pipe = subprocess.Popen(['cut', '-f', fields, fname],
+                            stdout=subprocess.PIPE)
+    with open('{}.cut'.format(fname[:fname.rfind('.')]), 'w') as f:
+        f.writelines(pipe.stdout)
+
+
+def dedup_files(base_name):
+    print 'Deduping and minimizing {}...'.format(base_name)
+    cut_name = '{}.cut'.format(base_name)
+    min_name = '{}_minimized.csv'.format(base_name)
+    with open(cut_name, 'r') as r, open(min_name, 'w') as w:
+        reader = DictReader(r, delimiter='\t')
+        writer = DictWriter(w, fieldnames=reader.fieldnames)
+        writer.writeheader()
+        file_data = set()
+        for row in reader:
+            row_data = tuple(row.values())
+            if row_data not in file_data:
+                file_data.add(row_data)
+                writer.writerow(row)
+    remove(cut_name)
 
 
 def main():
@@ -16,7 +44,7 @@ def main():
     parser.add_argument('-a', action='store_true', dest='all', default=False,
                         help='Flag to process all .csv/.txt directory files')
     parser.add_argument('-o', action='store', dest='fields', nargs='*',
-                        default=None, help='Space delimite list of fields')
+                        default=None, help='Space delimited list of fields')
 
     args = parser.parse_args()
 
@@ -27,12 +55,22 @@ def main():
         files = args.files
 
     if not args.fields:
-        print 'Get fields'
+        fieldnames = []
+        with open(files[0], 'r') as r:
+            reader = DictReader(r, delimiter='\t')
+            fieldnames = reader.fieldnames
+        for i, field_name in enumerate(fieldnames, start=1):
+            print '{}. {}'.format(i, field_name)
+        fields = raw_input('Comma delimited field #\'s to group by:')
     else:
-        fields = args.fields
+        fields = ','.join(args.fields)
 
     print files
     print fields
+
+    for fname in files:
+        cut_files(fname, fields)
+        dedup_files(fname[:fname.rfind('.')])
 
 if __name__ == '__main__':
     main()
