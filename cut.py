4007cb5663c93096d78623e3b3b62601fd16d902
Mike Jensen <jjensen.mike@gmail.com>
2014-04-22 10:32:57 -0400

    Remove test code

--- cut.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-pipe = subprocess.Popen(['cut', '-f', '1,22,26-29,76-84,86', '<file_name>'], stdout=subprocess.PIPE)
-with open('<output_file>', 'w') as f:
-    f.writelines(pipe.stdout)
